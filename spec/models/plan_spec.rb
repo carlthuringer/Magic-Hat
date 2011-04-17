ff107afab27c77aae61ecbef8290e5ab39a05cf0
Carl Thuringer <carlthuringer@gmail.com>
2011-04-17 14:18:10 -0500

    Remove whitespace. Added test for empty description validation

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe Plan do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "doesn't allow a Plan with no description" do
+    test_plan = Plan.new(:due_date => "12-25-2012")
+    test_plan.save.should == false
+  end
 end
