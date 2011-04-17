e77eb3bba2dcb4986e1ab758abe3c387b7cca278
Carl Thuringer <carlthuringer@gmail.com>
2011-04-16 21:57:44 -0500

    Fresh Rails app created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicHat::Application.config.secret_token = '6661eda71e1c18cd05ec079c7012839fe456323bfe6263689bd6b31ee3225b9c132d2ead989c55af51208e363007bb438a9f08160523cedb4838c9a5b2b8b8bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MagicHat::Application.config.session_store :cookie_store, :key => '_magic_hat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MagicHat::Application.config.session_store :active_record_store
