cec5c4e1df1d93f590d8fc32ea35c1ee607dd506
Carl Thuringer <carlthuringer@gmail.com>
2011-04-17 22:11:27 -0500

    Installed Webrat and dependencies.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-
+require 'webrat'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -25,4 +25,8 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
   
+end
+
+Webrat.configure do |config|
+  config.mode = :rails
 end
