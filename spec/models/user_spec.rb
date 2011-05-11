6952154518d9f03fadec190bd37f48dcbe10d55a
Carl Thuringer <carlthuringer@gmail.com>
2011-05-11 10:54:23 -0500

    Remove spec for user name length. The db limit is 255 characters but I'm no longer enforcing a short name.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,18 +15,12 @@ describe User do
   it "should create a new user instance given valid attributes" do
     User.create!(@attr)
   end
-  
+
   it "should require a name" do
     no_name_user = User.new(@attr.merge(:name => ''))
     no_name_user.should_not be_valid
   end
 
-  it "should reject a name that is too long" do
-    long_name = 'a' * 51
-    long_name_user = User.new(@attr.merge(:name => long_name))
-    long_name_user.should_not be_valid
-  end
-  
   it "should require an email address" do
     no_email_user = User.new(@attr.merge(:email => ''))
     no_email_user.should_not be_valid
