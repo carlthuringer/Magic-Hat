ffd54423ac9b50a0aeb1faa5c5911b7c7ad9bb9f
Carl Thuringer <carlthuringer@gmail.com>
2011-05-11 10:56:30 -0500

    I no longer require a user name at all. Spec removed.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,11 +16,6 @@ describe User do
     User.create!(@attr)
   end
 
-  it "should require a name" do
-    no_name_user = User.new(@attr.merge(:name => ''))
-    no_name_user.should_not be_valid
-  end
-
   it "should require an email address" do
     no_email_user = User.new(@attr.merge(:email => ''))
     no_email_user.should_not be_valid
