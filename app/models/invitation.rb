0d1a5c84bcc40e601a796d1c7baea6a879e59cae
Carl Thuringer <carlthuringer@gmail.com>
2011-07-31 17:02:39 -0500

    Add email validation to the Invitation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,9 @@
 class Invitation < ActiveRecord::Base
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex }
+
   belongs_to :group
 
 end
